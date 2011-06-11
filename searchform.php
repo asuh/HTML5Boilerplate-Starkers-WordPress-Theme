35164814438ef7ec9c0cf19d376f0c675a0afe50
Micah Cambre <micah@asuh.com>
2011-06-11 15:39:40 -0700

    Removed fieldset, added visuallyhidden to label so that it doesn't stay in the way

--- searchform.php
+++ searchform.php
@@ -1,7 +1,5 @@
 <form action="<?php echo home_url( '/' ); ?>" role="search" method="get" id="searchform">
-    <fieldset>
-        <label for="search">Search</label>
-        <input type="search" name="s" id="search" value="<?php the_search_query(); ?>" placeholder="search..." />
-        <input type="submit" id="searchsubmit" value="Search" />
-    </fieldset>
+	<label for="search" class="visuallyhidden">Search</label>
+	<input type="search" name="search" id="search" value="<?php the_search_query(); ?>" placeholder="search..." />
+	<input type="submit" id="searchsubmit" value="Search" />
 </form>
