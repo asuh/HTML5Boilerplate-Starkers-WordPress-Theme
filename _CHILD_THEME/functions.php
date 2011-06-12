ffcd2024890d9cf99a8dfe0b6275c219cbbe4f8c
Micah Cambre <micah@asuh.com>
2011-06-11 17:23:54 -0700

    Correct code to for child custom logo to replace parent code

--- _CHILD_THEME/functions.php
+++ _CHILD_THEME/functions.php
@@ -4,12 +4,12 @@
 	require_once(STYLESHEETPATH . '/boilerplate-admin/admin-menu.php');
 /* End Boilerplate Plug-in */
 
-// custom admin login logo
-function custom_login_logo() {
+// custom admin login logo for child theme
+function child_custom_login_logo() {
 	echo '<style type="text/css">
 	h1 a { background-image: url('.get_bloginfo('stylesheet_directory').'/img/custom-login-logo.png) !important; }
 	</style>';
 }
-add_action('login_head', 'custom_login_logo');
+add_action('login_head', 'child_custom_login_logo');
 
 ?>
