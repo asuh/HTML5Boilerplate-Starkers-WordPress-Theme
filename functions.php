e6b0a013491394f1285f155391fcdcaf6ef4e412
Micah Cambre <micah@asuh.com>
2011-06-11 17:16:27 -0700

    vCard widget added

--- functions.php
+++ functions.php
@@ -116,6 +116,99 @@ function boilerplate_robots() {
 	echo "Allow: /wp-content/uploads\n";
 	echo "Allow: /assets";
 }
+
+// vCard generator widget
+class boilerplate_vcard extends WP_Widget {
+
+	function boilerplate_vcard() {
+		$widget_ops = array('description' => 'Display a vCard');
+		parent::WP_Widget(false, __('vCard', 'boilerplate'), $widget_ops);      
+	}
+   
+	function widget($args, $instance) {  
+		extract($args);
+		$title = $instance['title'];
+		$street_address = $instance['street_address'];
+		$locality = $instance['locality'];
+		$region = $instance['region'];
+		$postal_code = $instance['postal_code'];
+		$country = $instance['country'];
+		$tel = $instance['tel'];
+		$email = $instance['email'];
+	?>
+		<?php echo $before_widget; ?>
+		<?php if ($title) echo $before_title . $title . $after_title; ?>  
+		<p class="vcard">
+			<a class="fn org url" href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a>
+			<div class="adr">
+				<?php if ($street_address) { ?><div class="street-address"><?php echo $street_address; ?></div><?php } ?>
+				<?php if ($locality) { ?><span class="locality"><?php echo $locality; ?></span>,<?php } ?>
+				<?php if ($region) { ?><span class="region"><?php echo $region; ?></span>&nbsp;<?php } ?>
+				<?php if ($postal_code) { ?><span class="postal-code"><?php echo $postal_code; ?></span><?php } ?>
+				<?php if ($country) { ?><div class="country-name"><?php echo $country; ?></div><?php } ?>
+			</div>
+			<?php if ($tel) { ?><div class="tel"><span class="type">Work: </span><span class="value"><?php echo $tel; ?></span></div><?php } ?>
+			<?php if ($tel) { ?><a class="email" href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a><?php } ?>
+		</p>        
+        
+        <?php echo $after_widget; ?>
+        
+	<?php
+	}
+	
+	function update($new_instance, $old_instance) {                
+		return $new_instance;
+	}
+
+	function form($instance) {
+		if (isset($instance['title'])) { $title = esc_attr($instance['title']); } else { $title = ''; }
+		if (isset($instance['street_address'])) { $street_address = esc_attr($instance['street_address']); } else { $street_address = ''; }
+		if (isset($instance['locality'])) { $locality = esc_attr($instance['locality']); } else { $locality = ''; }
+		if (isset($instance['region'])) { $region = esc_attr($instance['region']); } else { $region = ''; }
+		if (isset($instance['postal_code'])) { $postal_code = esc_attr($instance['postal_code']); } else { $postal_code = ''; }
+		if (isset($instance['country'])) { $country = esc_attr($instance['country']); } else { $country = ''; }
+		if (isset($instance['tel'])) { $tel = esc_attr($instance['tel']); } else { $tel = ''; }
+		if (isset($instance['email'])) { $email = esc_attr($instance['email']); } else { $email = ''; }
+	?>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title (optional):', 'boilerplate'); ?></label>
+			<input type="text" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $title; ?>" class="widefat" id="<?php echo $this->get_field_id('title'); ?>" />
+		</p>       
+		<p>
+			<label for="<?php echo $this->get_field_id('street_address'); ?>"><?php _e('Street Address:', 'boilerplate'); ?></label>
+			<input type="text" name="<?php echo $this->get_field_name('street_address'); ?>" value="<?php echo $street_address; ?>" class="widefat" id="<?php echo $this->get_field_id('street_address'); ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('locality'); ?>"><?php _e('City/Locality:', 'boilerplate'); ?></label>
+			<input type="text" name="<?php echo $this->get_field_name('locality'); ?>" value="<?php echo $locality; ?>" class="widefat" id="<?php echo $this->get_field_id('locality'); ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('region'); ?>"><?php _e('State/Region:', 'boilerplate'); ?></label>
+			<input type="text" name="<?php echo $this->get_field_name('region'); ?>" value="<?php echo $region; ?>" class="widefat" id="<?php echo $this->get_field_id('region'); ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('postal_code'); ?>"><?php _e('Zipcode/Postal Code:', 'boilerplate'); ?></label>
+			<input type="text" name="<?php echo $this->get_field_name('postal_code'); ?>" value="<?php echo $postal_code; ?>" class="widefat" id="<?php echo $this->get_field_id('postal_code'); ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('country'); ?>"><?php _e('Country:', 'boilerplate'); ?></label>
+			<input type="text" name="<?php echo $this->get_field_name('country'); ?>" value="<?php echo $country; ?>" class="widefat" id="<?php echo $this->get_field_id('country'); ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('tel'); ?>"><?php _e('Telephone:', 'boilerplate'); ?></label>
+			<input type="text" name="<?php echo $this->get_field_name('tel'); ?>" value="<?php echo $tel; ?>" class="widefat" id="<?php echo $this->get_field_id('tel'); ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('email'); ?>"><?php _e('Email:', 'boilerplate'); ?></label>
+			<input type="text" name="<?php echo $this->get_field_name('email'); ?>" value="<?php echo $email; ?>" class="widefat" id="<?php echo $this->get_field_id('email'); ?>" />
+		</p>                                   
+	<?php
+	}
+} 
+
+register_widget('boilerplate_vcard');
+
+
  
 /**
 * END TLD scripts
