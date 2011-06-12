08e963892c0360263f5bf601c98c06a1ae897e94
Micah Cambre <micah@asuh.com>
2011-06-11 17:21:21 -0700

    Add custom logo code and img to Child Theme

--- _CHILD_THEME/functions.php
+++ _CHILD_THEME/functions.php
@@ -3,5 +3,13 @@
 /* Begin Boilerplate Admin */
 	require_once(STYLESHEETPATH . '/boilerplate-admin/admin-menu.php');
 /* End Boilerplate Plug-in */
-	
+
+// custom admin login logo
+function custom_login_logo() {
+	echo '<style type="text/css">
+	h1 a { background-image: url('.get_bloginfo('stylesheet_directory').'/img/custom-login-logo.png) !important; }
+	</style>';
+}
+add_action('login_head', 'custom_login_logo');
+
 ?>
