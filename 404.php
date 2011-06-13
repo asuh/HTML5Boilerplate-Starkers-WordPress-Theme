0dcd6ddcfa7609f153832be5bc309bd4cd167e34
Micah Cambre <micah@asuh.com>
2011-06-13 16:23:23 -0700

    Delete extra >

--- 404.php
+++ 404.php
@@ -7,7 +7,7 @@
  * @since Boilerplate 1.0
  */
 
-get_header(); ?>>
+get_header(); ?>
 			<article id="post-0" class="post error404 not-found" role="main">
 				<h1><?php _e( 'Not Found', 'boilerplate' ); ?></h1>
 				<p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'boilerplate' ); ?></p>
